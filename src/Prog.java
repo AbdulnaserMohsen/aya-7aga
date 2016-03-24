9936044e7908022de7355b66e9c3e6b72a997c02
User <User@172.31.42.39>
2016-03-24 11:42:20 +0200

    method welcome

--- src/Prog.java
+++ src/Prog.java
@@ -3,7 +3,12 @@ public class Prog {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		welc();
 
 	}
 
+	static void welc()
+	{
+		System.out.println("welcome");
+	}
 }
